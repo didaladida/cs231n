c34323bf8adee10f2e4b3625568b6ad4a2184ced
sulixin <sulixinict@gmail.com>
2017-01-04 11:56:14 +0800

    fix knn bug && add linear svm

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -33,15 +33,17 @@ def svm_loss_naive(W, X, y, reg):
         continue
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
+        dW[:,j] += X[i].T        
+        dW[:,y[i]] -= X[i].T
         loss += margin
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
-
+  dW /= num_train
   # Add regularization to the loss.
   loss += 0.5 * reg * np.sum(W * W)
-
+    
   #############################################################################
   # TODO:                                                                     #
   # Compute the gradient of the loss function and store it dW.                #
@@ -69,7 +71,18 @@ def svm_loss_vectorized(W, X, y, reg):
   # Implement a vectorized version of the structured SVM loss, storing the    #
   # result in loss.                                                           #
   #############################################################################
-  pass
+  scores = X.dot(W)
+  # print scores.shape
+  correct_class_scores = scores[range(scores.shape[0]),y]
+  # print correct_class_scores.shape
+  scores = np.maximum(scores.T - correct_class_scores + 1,0)
+  scores = scores.T
+  scores[range(scores.shape[0]),y] = 0
+  loss = np.sum(scores)/X.shape[0]
+  scores[scores>0] = 1
+  scores[range(scores.shape[0]),y] = 0 - np.sum(scores,axis=1)
+  dW += X.T.dot(scores)
+  dW /= X.shape[0]
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
